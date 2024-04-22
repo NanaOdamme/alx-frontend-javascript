24004b29cd13c7dbf7fc80e27000fc645169f256
NanaOdamme <eronastyles@gmail.com>
2024-04-22 10:38:39 +0000

    Advanced tasks completed

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,11 @@
+export default function createIteratorObject(report) {
+    function* employeeGenerator() {
+      for (const employees of Object.values(report.allEmployees)) {
+        for (const employee of employees) {
+          yield employee;
+        }
+      }
+    }
+  
+    return employeeGenerator();
+  }
