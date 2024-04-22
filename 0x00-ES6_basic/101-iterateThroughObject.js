c689bc93853c58a763b38ccbbac84f2ce6715374
NanaOdamme <eronastyles@gmail.com>
2024-04-22 12:32:19 +0000

    updated

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++
@@ -1,9 +0,0 @@
-export default function iterateThroughObject(reportWithIterator) {
-    const employeesStringArray = [];
-  
-    for (const employee of reportWithIterator) {
-      employeesStringArray.push(employee);
-    }
-  
-    return employeesStringArray.join(' | ');
-  }
