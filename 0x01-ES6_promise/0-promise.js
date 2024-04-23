9111b6e4d3e3b942b7cfc74810b76ad4aa3fd9d3
NanaOdamme <eronastyles@gmail.com>
2024-04-23 09:01:43 +0000

    updated

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,9 @@
+
+export default function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const responseData = { message: "API response data" };
+        resolve(responseData);
+      }, 2000);
+    });
+  }
